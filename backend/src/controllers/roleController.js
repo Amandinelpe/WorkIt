c4771128a43f29b09fecec647230a68f37b5b859
Amandine Leporace <leporace.amandine@gmail.com>
2022-12-16 11:13:15 +0100

    sauvegarde

---
+++ backend/src/controllers/roleController.js
@@ -0,0 +1,19 @@
+const roleModel = require("../models/roleModel");
+
+const roleController = {
+  getAllRoles: (req, res) => {
+    roleModel
+      .findAll()
+      .then(([roles]) => res.status(200).send(roles))
+      .catch((err) => console.error(err));
+  },
+  getRoleById: (req, res) => {
+    const { id } = req.params;
+    roleModel
+      .findOne(id)
+      .then(([role]) => res.status(200).send(role))
+      .catch((err) => console.error(err));
+  },
+};
+
+module.exports = roleController;
