22e6fe6f1601f66633cdfa6e0391a34ba55c9926
paul battault <paulbattault@gmail.com>
2022-12-16 09:41:52 +0100

    router user done

---
+++ backend/src/router/userRouter.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const userRouter = express.Router();
+
+const userControler = require("../controllers/userController");
+
+userRouter.get("/", userControler.getAllUsers);
+userRouter.get("/:id", userControler.getUserById);
+userRouter.post("/", userControler.createUser);
+
+module.exports = userRouter;
