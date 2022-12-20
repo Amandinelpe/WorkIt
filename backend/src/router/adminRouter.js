c207504b1a0f21f61719836d08ed6459fc19ca0d
Mbirepinte <mickael.birepinte@gmail.com>
2022-12-20 12:22:29 +0100

    delete dead code

--- backend/src/router/adminRouter.js
+++ backend/src/router/adminRouter.js
@@ -6,6 +6,6 @@ const adminController = require("../controllers/adminController");
 
 adminRouter.get("/", adminController.getAllAdmins);
 adminRouter.get("/:id", adminController.getAdminById);
-/* userRouter.post("/", userControler.createUser); */
+
 
 module.exports = adminRouter;
