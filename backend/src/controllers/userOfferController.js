dde5492b39f65ad636d888545e083e14bddb8fbf
Mbirepinte <mickael.birepinte@gmail.com>
2023-01-04 16:20:18 +0100

    change to create admin branch

--- backend/src/controllers/userOfferController.js
+++ backend/src/controllers/userOfferController.js
@@ -14,32 +14,6 @@ const userOfferController = {
       .then(([userOffer]) => res.status(200).send(userOffer))
       .catch((err) => next(err));
   },
-  const findOne = (id) => {
-  return db
-    .promise()
-    .query("SELECT * FROM user where id = ?", [id])
-    .then(([user]) => user);
 };
-
-const createOne = ({
-  role_id,
-  firstname,
-  lastname,
-  job_id,
-  city,
-  email,
-  password,
-}) => {
-  return db
-    .promise()
-    .query(
-      "INSERT INTO user (role_id, firstname, lastname, job_id, city, email, password) VALUES (?, ?, ?, ?, ?, ?, ?)",
-      [role_id, firstname, lastname, job_id, city, email, password]
-    )
-    .then((user) => user);
-};
-
-};
-
 
 module.exports = userOfferController;
