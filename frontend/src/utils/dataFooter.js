d284fc369d0e4eff0ad78fefd41ccb5b682ba430
Amandine Leporace <leporace.amandine@gmail.com>
2023-01-05 09:28:53 +0100

    Footer terminé

--- frontend/src/utils/dataFooter.js
+++ frontend/src/utils/dataFooter.js
@@ -1,12 +1,31 @@
 const dataFooter = {
-  title: "Contact",
-  paragraphs: [
+  infosSite: [
     {
-      lignes: [
-        "Tél: +33 (0)2 85 52 26 33 ",
-        "Mail : contact@externatic.fr",
-        "Adresse : 1 rue Racine – 44000 NANTES – France",
-      ],
+      titre: "Fiches métiers",
+      lien: "#",
+    },
+    {
+      titre: "Mentions légales",
+      lien: "#",
+    },
+    {
+      titre: "Politique de confidentialité",
+      lien: "#",
+    },
+    {
+      titre: "Politique de cookies",
+      lien: "#",
+    },
+    {
+      titre: "Plan du site",
+      lien: "#",
     },
   ],
+  infosContact: [
+    "Tél: +33 (0)2 85 52 26 33 ",
+    "Mail : contact@externatic.fr",
+    "Adresse : 1 rue Racine – 44000 NANTES – France",
+  ],
 };
+
+export default dataFooter;
