c8a096f55253f4df43e8656b76743f0663ea8710
Luc Jaubert <lucjaubert@gmail.com>
2023-01-05 13:17:02 +0100

    avancées dashboard

--- frontend/src/components/BoxFavorites.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "../styles/BoxFavorites.css";
-
-const BoxFavorites = () => {
-  return <div className="my_favorites" />;
-};
-
-export default BoxFavorites;
--- frontend/src/pages/DashboardCandidate.jsx
+++ frontend/src/pages/DashboardCandidate.jsx
@@ -1,16 +1,44 @@
-import React from "react";
+import { React, useEffect, useState } from "react";
 import NavBar from "../components/NavBar";
 import HelloButton from "../components/HelloButton";
 import BoxCandidate from "../components/BoxCandidate";
+import { GetFavoritesOffers } from "../utils/GetFavoritesOffers";
+import Offer from "../components/Offer";
 import "../styles/DashboardCandidate.css";
 
 const DashboardCandidate = () => {
+  const [favoritesOffers, setFavoritesOffers] = useState([]);
+
+  const getFavoritesOffers = async () => {
+    setFavoritesOffers(await GetFavoritesOffers());
+  };
+
+  useEffect(() => {
+    getFavoritesOffers();
+  }, []);
+
   return (
     <div>
       <NavBar />
       <HelloButton />
       <div className="mydashboard">
         <BoxCandidate />
+      </div>
+      <div className="mainPage_favorites_body">
+        <div className="mainPage_favorites_offers">
+          <div className="all_favorites_offers_titleblock">
+            <h2 className="all_favorites_offers_title">Mes coups de coeur</h2>
+          </div>
+          <div className="favorites_offers_body">
+            {favoritesOffers.map((offer) => (
+              <Offer date={offer.date} />
+            ))}
+            <button type="button" className="all_favorites_offers_button">
+              {" "}
+              Voir plus d'offres{" "}
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
