dbe824634027e09d9bb0aa9ad90d4214ac5fb58b
Olga Yasnopolskaya <olga_yasn@hotmail.com>
2023-01-05 22:15:19 +0100

    worked on back of user

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -14,6 +14,7 @@ const userController = {
       .then(([user]) => res.status(200).send(user))
       .catch((err) => next(err));
   },
+
   createUser: (req, res, next) => {
     userModel
       .createOne(req.body)
@@ -23,6 +24,19 @@ const userController = {
         } else {
           res.send("User is not created");
         }
+      })
+      .catch((err) => next(err));
+  },
+
+  deleteUser: (req, res, next) => {
+    const { id } = req.params;
+    userModel
+      .deleteOne(id)
+      .then((response) => {
+        if (response.affectedRows !== 1) {
+          return res.status(404).send(`user ${id} not found`);
+        }
+        return res.status(200).send(`user ${id} deleted`);
       })
       .catch((err) => next(err));
   },
