b8e2b5dcc73620ab1880dd632646a003896c5d50
Olga Yasnopolskaya <olga_yasn@hotmail.com>
2023-01-18 12:30:26 +0100

    worked on forgotten email

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -3,8 +3,9 @@
 
 const { validationResult } = require("express-validator");
 const argon2 = require("argon2");
-const jwt = require("jsonwebtoken");
+// const nodemailer = require("nodemailer");
 const userModel = require("../models/userModel");
+const { jwtSign } = require("../middlewares/jwt");
 
 const userController = {
   getAllUsers: (req, res, next) => {
@@ -77,9 +78,8 @@ const userController = {
           } = user;
 
           if (await argon2.verify(hashedPassword, password)) {
-            const token = jwt.sign(
+            const token = jwtSign(
               { id, userEmail, firstname, lastname, role_id },
-              process.env.JWT_AUTH_SECRET,
               { expiresIn: "1h" }
             );
             res
@@ -113,6 +113,57 @@ const userController = {
       })
       .catch((err) => next(err));
   },
+
+  // resetPassword: (req, res, next) => {
+  //   const { email } = req.body;
+  //   userModel.findByEmail(email).then((user) => {
+  //     if (!user) {
+  //       return res.status(404).send("User not found");
+  //     }
+  //     const token = jwtSign({ id: user.id, email }, { expiresIn: "15m" });
+
+  //     nodemailer.createTestAccount((err, account) => {
+  //       if (err) {
+  //         console.error(`Failed to create a testing account. ${err.message}`);
+  //         return process.exit(1);
+  //       }
+
+  //       console.log("Credentials obtained, sending message...");
+  //       console.log(account, "COUCOU");
+
+  //       // // Create a SMTP transporter object
+  //       // const transporter = nodemailer.createTransport({
+  //       //   host: account.smtp.host,
+  //       //   port: account.smtp.port,
+  //       //   secure: account.smtp.secure,
+  //       //   auth: {
+  //       //     user: account.user,
+  //       //     pass: account.pass,
+  //       //   },
+  //       // });
+
+  //       // Message object
+  //       // const message = {
+  //       //   from: "Sender Name <sender@example.com>",
+  //       //   to: "Recipient <recipient@example.com>",
+  //       //   subject: "Nodemailer is unicode friendly ✔",
+  //       //   text: "Hello to myself!",
+  //       //   html: "<p><b>Hello</b> to myself!</p>",
+  //       // };
+
+  //       // transporter.sendMail(message, (err, info) => {
+  //       //   if (err) {
+  //       //     console.log(`Error occurred. ${err.message}`);
+  //       //     return process.exit(1);
+  //       //   }
+
+  //       //   console.log("Message sent: %s", info.messageId);
+  //       //   // Preview only available when sending through an Ethereal account
+  //       //   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  //       // });
+  //     });
+  //   });
+  // },
 };
 
 module.exports = userController;
