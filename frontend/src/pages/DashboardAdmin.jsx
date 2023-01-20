5dd055736a61963ad85d8f3bc446b281f5889da9
Amandine Leporace <leporace.amandine@gmail.com>
2023-01-20 16:31:56 +0100

    save

--- frontend/src/pages/DashboardAdmin.jsx
+++ frontend/src/pages/DashboardAdmin.jsx
@@ -1,39 +1,38 @@
-// import { React, useState } from "react";
-// import NavBar from "../components/NavBar";
-// import BoxAdmin from "../components/BoxAdmin";
-// // import HelloButton from "../components/HelloButton";
-// import Footer from "../components/Footer";
-// import "../styles/BoxAdmin.css";
+import { React, useState } from "react";
+import NavBar from "../components/NavBar";
+import BoxAdmin from "../components/BoxAdmin";
+import Footer from "../components/Footer";
+import "../styles/BoxAdmin.css";
+import MyProfile from "../components/MyProfile";
 
-// const DashboardAdmin = () => {
-//   const [content, setContent] = useState("dashboard");
+const DashboardAdmin = () => {
+  const [content, setContent] = useState("dashboard");
 
-//   const handleContent = (ctn) => {
-//     setContent(ctn);
-//   };
+  const handleContent = (ctn) => {
+    setContent(ctn);
+  };
 
-//   const renderSwitch = () => {
-//     switch (content) {
-//       case "my-profile":
-//         return <MyProfile />;
-//       default:
-//         return <Dashboard />;
-//     }
-//   };
+  const renderSwitch = () => {
+    switch (content) {
+      case "my-profile":
+        return <MyProfile />;
+      default:
+        return <DashboardAdmin />;
+    }
+  };
 
-//   return (
-//     <div>
-//       <NavBar />
-//       {/* <HelloButton /> */}
-//       <div className="mydashboardadmin_body">
-//         <BoxAdmin handleContent={handleContent} />
-//       </div>
-//       {/* {renderSwitch()} */}
-//       <div>
-//         <Footer />
-//       </div>
-//     </div>
-//   );
-// };
+  return (
+    <div>
+      <NavBar />
+      <div className="mydashboardadmin_body">
+        <BoxAdmin handleContent={handleContent} />
+      </div>
+      {renderSwitch()}
+      <div>
+        <Footer />
+      </div>
+    </div>
+  );
+};
 
-// export default DashboardAdmin;
+export default DashboardAdmin;
