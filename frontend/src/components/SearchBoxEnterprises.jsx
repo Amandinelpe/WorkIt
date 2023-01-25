66ea44ae2c9c84697290b945e1e4049b411e40ea
Luc Jaubert <lucjaubert@gmail.com>
2023-01-25 12:34:12 +0100

    avancées feature

--- frontend/src/components/SearchBoxEnterprises.jsx
+++ frontend/src/components/SearchBoxEnterprises.jsx
@@ -1,63 +1,41 @@
 // import { React, useEffect, useState } from "react";
 // import PropTypes from "prop-types";
-// import { GetAllFirms } from "../apis/jobApi";
+// import { GetAllFirms } from "../utils/getAllEnterprises";
 // import "../styles/SearchBoxEnterprises.css";
-// import search from "../assets/img/search.png";
 
 // const SearchBoxEnterprises = ({ setSelectedFirm }) => {
 //   const [firm, setFirm] = useState([]);
 
 //   const getFirms = () => {
-//     GetAllCities().then((res) => {
-//       setCities(res.data);
+//     GetAllFirms().then((res) => {
+//       setFirm(res.data);
 //     });
 //   };
 
 //   useEffect(() => {
-//     loadJobs();
-//     getCities();
+//     getFirms();
 //   }, []);
 
 //   return (
-//     <div className="searchBar">
-//       <div className="inputBox">
-//         <label htmlFor="firm _select">
-//           <select
-//             id="job_select"
-//             onChange={(e) => setSelectedJob(e.target.value)}
-//           >
-//             <option disabled selected value>
-//               Quoi?{" "}
-//             </option>
-//             <option value="">Tous les postes</option>
-//             {job.map((work) => (
-//               <option value={work.id}> {work.job_title}</option>
-//             ))}
-//           </select>
-//         </label>
+//     <>
+//       <div className="searchBoxEnterprises">
+//         <input
+//           type="text"
+//           name="searchBarEnterprise"
+//           id="searchBarEnterprise"
+//           placeholder="Rechercher une entreprise"
+//           // onChange={(e) => setSelectedFirm(e.target.value)}
+//         />
 //       </div>
-//       <div className="inputBox">
-//         <label htmlFor="city_select">
-//           <select id="city_select" onChange={(e) => setCity(e.target.value)}>
-//             <option disabled selected value>
-//               {" "}
-//               Ou?{" "}
-//             </option>
-//             <option value="">Partout</option>
-//             {cities.map((city) => (
-//               <option value={city.firm_city}>{city.firm_city} </option>
-//             ))}
-//           </select>
-//         </label>
+//       <div className="search_resultats_enterprises">
+//         <div className="search_resultat_enterprise">Données</div>
 //       </div>
-//       <img src={search} alt="search" className="logo" />
-//     </div>
+//     </>
 //   );
 // };
 
 // export default SearchBoxEnterprises;
 
 // SearchBoxEnterprises.propTypes = {
-//   setSelectedJob: PropTypes.func.isRequired,
-//   setCity: PropTypes.func.isRequired,
+//   setSelectedFirm: PropTypes.func.isRequired,
 // };
