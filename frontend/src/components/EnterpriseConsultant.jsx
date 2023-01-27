86ace5535656b43e5adb342eaa31526d45a76556
Luc Jaubert <lucjaubert@gmail.com>
2023-01-27 16:02:42 +0100

    Ok

--- frontend/src/components/EnterpriseConsultant.jsx
+++ frontend/src/components/EnterpriseConsultant.jsx
@@ -4,15 +4,7 @@ import search from "../assets/img/logo_search_enterprises.png";
 import GridEntreprises from "./GridEntreprises";
 import { GetFirmOffer } from "../apis/firmOfferApi";
 
-// loadMessages(frMessages, "fr-FR");
-
-// const initialDataState = {
-//   skip: 0,
-//   take: 10,
-// };
-
 const EnterpriseConsultant = () => {
-  // const [page, setPage] = React.useState(initialDataState);
   const [myEnterprises, setMyEnterprises] = useState([]);
   const [searchValue, setSearchValue] = useState("");
 
@@ -35,11 +27,6 @@ const EnterpriseConsultant = () => {
   const filteredFirms = myEnterprises.filter((firm) => {
     return firm.name.toLowerCase().includes(searchValue.toLowerCase());
   });
-
-  // const handleRowClick = (data) => {
-  //   const firmId = data.firm_id;
-  //   return `/FicheEntreprise/${firmId}`;
-  // };
 
   return (
     <div className="container_enterprise_body">
@@ -66,56 +53,6 @@ const EnterpriseConsultant = () => {
           </div>
           <div className="dashboard_body">
             <GridEntreprises filteredFirms={filteredFirms} />
-
-            {/* <div className="dashboard_enterprises">
-              <LocalizationProvider language="fr-FR">
-                <IntlProvider locale="fr">
-                  <Grid
-                    className="grid_enterprise"
-                    data={filteredFirms.slice(page.skip, page.take + page.skip)}
-                    skip={page.skip}
-                    take={page.take}
-                    total={filteredFirms.length}
-                    pageable
-                    onPageChange={pageChange}
-                  >
-                    <GridColumn
-                      title="Id entreprise"
-                      field="firm_id"
-                      width="70vw"
-                    />
-                    <GridColumn
-                      title="Nom de l'entreprise"
-                      field="name"
-                      width="285vw"
-                    />
-                    <GridColumn
-                      title="Téléphone"
-                      field="contact_phone"
-                      width="200vw"
-                    />
-                    <GridColumn title="Email" field="email" width="200vw" />
-                    <GridColumn title="Ville" field="city" width="200vw" />
-                    <GridColumn
-                      title="Nombre d'annonces"
-                      field="nbreoffers"
-                      width="150vw"
-                    />
-                    <GridColumn
-                      title="Actions"
-                      width="100vw"
-                      cell={(props) => (
-                        <td>
-                          <Link to={handleRowClick(props.dataItem)}>
-                            Voir la fiche
-                          </Link>
-                        </td>
-                      )}
-                    />
-                  </Grid>
-                </IntlProvider>
-              </LocalizationProvider>
-            </div>  */}
           </div>
         </div>
       </div>
