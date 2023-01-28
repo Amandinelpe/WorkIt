c5c128c85e32bd28cbf86530f146f2f2710acb63
Mbirepinte <mickael.birepinte@gmail.com>
2023-01-28 09:44:01 +0100

    for luc

---
+++ frontend/src/components/DashboardFirmOffers.jsx
@@ -0,0 +1,51 @@
+import { React, useEffect, useState} from "react";
+import Offer from "./Offer";
+import { GetFirmOffer } from "../apis/firmApi"; 
+import OfferEmpty from "./OfferEmpty";
+
+const DashboardFirmOffers = ({id}) => {
+const [firmOffers, setFirmOffers] = useState([]);
+  const [reload, setReload] = useState(true);
+
+  const getFirmOffers = async () => {
+    await GetFirmOffer(id).then((res) => setFirmOffers(res.data)).catch ((err) => console.warn(err));
+  }
+  console.log(id, "id")
+  console.log(firmOffers, "firmOffers")
+  console.log(reload, "reload");
+  useEffect(() => {
+    getFirmOffers()
+  }, [reload]);
+
+  return (
+    <div>
+      <div className="my_favorites_body">
+        <div className="my_favorites_offers">
+          <div className="all_favorites_offers_titleblock">
+            <h2 className="all_favorites_offers_title">Mes coups de coeur</h2>
+          </div>
+          <div className="my_favorites_offers_body">
+            {firmOffers.length === 0 ? (
+              <OfferEmpty />
+            ) : (
+              firmOffers.map((offer) => (
+                <Offer
+                  date={offer.date}
+                  firm={offer.name}
+                  title={offer.title}
+                  logo={offer.logo_url}
+                  city={offer.firm_city}
+                  id={offer.id}
+                  setReload={setReload}
+                  reload={reload}
+                />
+              ))
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardFirmOffers;
--- frontend/src/components/FirmForm.jsx
+++ frontend/src/components/FirmForm.jsx
@@ -1,34 +1,30 @@
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import NavBar from "./NavBar";
-import Offer from "./Offer";
-import { GetFirmData, GetFirmOffer } from "../apis/firmApi"; 
+import { GetFirmData } from "../apis/firmApi"; 
 import { useEffect } from "react";
+import DashboardFirmOffers from "./DashboardFirmOffers";
+import "../styles/FirmForm.css";
 
 
 
-const FirmForm = (updateFirm, firmOffer) => {
+const FirmForm = (firmOffer) => {
   const {id} = useParams();
   const [firm, setFirm] = useState([]);
   const [message, setMessage] = useState();
   const [disableSaveButton, setDisableSaveButton] = useState(true);
-  const [firmOffers, setFirmOffers] = useState([]);
-  console.log(id, "id")
+ 
+  
   const getFirmData = async () => {
-    await GetFirmData(id).then((res) => setFirm(res.data)).catch ((err) => console.log(err));
+    await GetFirmData(id).then((res) => setFirm(res.data)).catch ((err) => console.warn(err));
   }
 
-  const getFirmOffer = async () => {
-    await GetFirmOffer(id).then((res) => setFirmOffers(res.data)).catch ((err) => console.log(err));
-  }
 
   useEffect(() => {
     getFirmData();
-    getFirmOffer();
   },[])
 
-  console.log(firmOffers, "firmOffers");
-  console.log(firm, "firm");
+ 
   const handleChange = (e, customValue) => {
     setMessage(null);
     setDisableSaveButton(false);
@@ -42,122 +38,119 @@ const FirmForm = (updateFirm, firmOffer) => {
   return (
     <div>
       <NavBar/>
-    <div className="firm_form_body">
-      <div className="firm_form_block">
-        <div className="box_box_firm">
-          <div className="box_firm_title">
-            <h1>Fiche entreprise</h1>
-          </div>
-          <div className="box_firm_body">
-            <div className="informations-entreprise">
-              <div>
-                <div className="first_line_details">
-                  <div className="entreprise">
-                    <p className="label-entreprise">{firm.name}</p>
-                  </div>
-                  <div>
-                    <div className="account_state">
-                      <label>
-                        Etat compte
-                        <input
-                          type="text"
-                          name="account_state"
-                          className="small-input"
-                          />
-                      </label>
-                    </div>
-                  </div>
-                  <div className="Id_firm">
-                    <label>
-                      Id client
-                      <input
-                        type="text"
-                        name="name"
-                        className="small-input"
-                        value={firm.firm_id}
-                        onChange={handleChange}
-                        />
-                    </label>
-                  </div>
-                  <div className="second_line_details">
-                    <label>
-                      Secteur
-                      <input
-                        type="text"
-                        name="niveau-qualification"
-                        className="small-input"
-                        />
-                    </label>
-                    <label>
-                      Mon site internet
-                      <input
-                        type="text"
-                        name="website"
-                        className="small-input"
-                        value={firm.website}
-                        onChange={handleChange}
-                      />
-                    </label>
-                  </div>
-                  <div className="third_line-details">
-                    <label>
-                      Nombre de salariés
-                      <input
-                        type="text"
-                        name="nombre de salariés"
-                        className="small-input"
-                        />
-                    </label>
-                    <label>
-                      Email
-                      <input
-                        type="text"
-                        name="email"
-                        className="small-input"
-                        value={firm.email}
-                        />
-                    </label>
-                  </div>
-                  <div className="logo_enterprise_form">
+      <div className="firm_form">
+            <div className="box_firm_body">
+        
+                 <h1>Fiche entreprise</h1>
+               <div className="informations-entreprise">
                     <div>
-                      <p>Logo</p>
-                    </div>
-                  </div>
-                  <div className="fourth_line_details">
-                    <label>
-                      Adresse
-                      <input
-                        type="text"
-                        name="adress"
-                        className="small-input"
-                        value={firm.adress}
-                        onChange={handleChange}
-                      />
-                    </label>
-                    <div className="fifth_line_details">
-                      <label>
-                        Ville
-                        <input
-                          type="text"
-                          name="city"
-                          className="small-input"
-                          value={firm.city}
-                          onChange={handleChange}
-                          />
-                      </label>
-                    </div>
+                        <div className="first_line_details">
+                            <div className="entreprise">
+                                <p className="label-entreprise">{firm.name}</p>
+                            </div>
+                                  <div>
+                                      <div className="account_state">
+                                          <label>
+                                            Etat compte
+                                            <input
+                                              type="text"
+                                              name="account_state"
+                                              className="small-input"
+                                              />
+                                          </label>
+                                        </div>
+                                    </div>
+                                   <div className="Id_firm">
+                                      <label>
+                                        Id client
+                                        <input
+                                          type="text"
+                                          name="name"
+                                          className="small-input"
+                                          value={firm.firm_id}
+                                          onChange={handleChange}
+                                          />
+                                      </label>
+                                   </div>
+                                   <div className="second_line_details">
+                                      <label>
+                                        Secteur
+                                        <input
+                                          type="text"
+                                          name="niveau-qualification"
+                                          className="small-input"
+                                          />
+                                      </label>
+                                        <label>
+                                          Mon site internet
+                                          <input
+                                            type="text"
+                                            name="website"
+                                            className="small-input"
+                                            value={firm.website}
+                                            onChange={handleChange}
+                                            />
+                                        </label>
+                                   </div>
+                                   <div className="third_line-details">
+                                        <label>
+                                          Nombre de salariés
+                                          <input
+                                            type="text"
+                                            name="nombre de salariés"
+                                            className="small-input"
+                                            />
+                                        </label>
+                                        <label>
+                                          Email
+                                          <input
+                                            type="text"
+                                            name="email"
+                                            className="small-input"
+                                            value={firm.email}
+                                            />
+                                        </label>
+                                   </div>
+                                    <div className="logo_enterprise_form">
+                                        <div>
+                                          <p>Logo</p>
+                                        </div>
+                                    </div>
+                                      <div className="fourth_line_details">
+                                          <label>
+                                            Adresse
+                                            <input
+                                              type="text"
+                                              name="adress"
+                                              className="small-input"
+                                              value={firm.adress}
+                                              onChange={handleChange}
+                                              />
+                                          </label>
+                                      <div className="fifth_line_details">
+                                        <label>
+                                          Ville
+                                          <input
+                                            type="text"
+                                            name="city"
+                                            className="small-input"
+                                            value={firm.city}
+                                            onChange={handleChange}
+                                            />
+                                        </label>
+                                      </div>
+                                     </div>
+                                    </div>
+                                
+                                            </div>
+                            
+                            </div>
+                          </div>
                     <div className="current_offers">
-                      <Offer value={firmOffer} />
+                      <DashboardFirmOffers id={id}/>
                     </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
                         </div>
+    </div>
   );
 };
 
