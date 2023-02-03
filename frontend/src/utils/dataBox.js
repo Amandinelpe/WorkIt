9d5b7cb0ebc51e7d0b1fb41e80854a96660a9fcb
Mbirepinte <mickael.birepinte@gmail.com>
2023-02-03 09:35:12 +0100

    new component added

--- frontend/src/utils/dataBox.js
+++ frontend/src/utils/dataBox.js
@@ -3,11 +3,11 @@ const dataBox = [
     id: 1,
     title: "Mon espace",
     sections: [
+      "Mes nouvelles offres",
       "Mes coups de coeur",
       "Mes candidatures",
       "Mes informations personnelles",
       "Je rentre en contact avec un.e consultant.e",
-      "Mes alertes",
       "Gérer mes alertes",
     ],
     link: "dashboard",
