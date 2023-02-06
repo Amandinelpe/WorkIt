66fe39f1610769ed1657b50d20b42b74fdbf3b1e
Luc Jaubert <lucjaubert@gmail.com>
2023-02-06 11:23:34 +0100

    feature ok

--- backend/src/router/firmRouter.js
+++ backend/src/router/firmRouter.js
@@ -7,5 +7,7 @@ firmRouter.get("/", firmController.getAllFirms);
 firmRouter.get("/offer", firmController.getFirmOffer);
 firmRouter.post("/createFirm", firmController.createFirm);
 firmRouter.get("/:id", firmController.getFirmById);
+firmRouter.delete("/deleteFirm/:id", firmController.deleteFirm);
+firmRouter.put("/updateFirm/:id", firmController.updateFirm);
 
 module.exports = firmRouter;
