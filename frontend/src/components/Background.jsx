6311973775f71113aac45c09b42e77d608250715
Mbirepinte <mickael.birepinte@gmail.com>
2023-02-07 12:57:57 +0100

    bg changed

--- frontend/src/components/Background.jsx
+++ frontend/src/components/Background.jsx
@@ -1,19 +1,21 @@
 import React, { useContext } from "react";
+import ReactDOM from "react-dom";
 import { authContext } from "../context/AuthContext";
 import BG from "../assets/img/workit02-fond.png";
 
-const Background = () => {
+const Background = ({user}) => {
   const { auth } = useContext(authContext);
-  return (
+  return ReactDOM.createPortal(
     <div
       className={
-        auth.data && auth.data.role_id === 2
+        auth.data && auth.data.role_id === 2 || user === "consultant"
           ? "site_background"
           : "site_background_normal"
       }
     >
       <img src={BG} alt="background" />
-    </div>
+    </div>,
+    document.getElementById("root")
   );
 };
 
